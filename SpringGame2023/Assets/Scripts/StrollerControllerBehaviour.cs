2ea23a4a5c52a157446f37dd52bd160aa18a70e9
Hank Weiss <10811528@my.uvu.edu>
2023-01-28 18:20:09 -0700

    setting up strollerController to detect swipes and jump position using input system

--- SpringGame2023/Assets/Scripts/StrollerControllerBehaviour.cs
+++ SpringGame2023/Assets/Scripts/StrollerControllerBehaviour.cs
@@ -1,8 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StrollerControllerBehaviour : VehicleBase
 {
-    
+    private Transform transformObj;
+
+    public void JumpLeft()
+    {
+        Debug.Log("Swipe Detected");
+        transform.position.Set(-1, transformObj.position.y, transformObj.position.z);
+    }
 }
