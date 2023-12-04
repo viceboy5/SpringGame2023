22116010681b4286fa8aae91ada8b1f5dc4318b6
Hank <10811528@my.uvu.edu>
2023-12-04 14:43:41 -0700

    starting tiles randomize

--- SpringGame2023/Assets/Scripts/Pooling.cs
+++ SpringGame2023/Assets/Scripts/Pooling.cs
@@ -41,4 +41,44 @@ public class Pooling : MonoBehaviour
         inactivePool.Add(obj);
         obj.SetActive(false);
     }
+    
+    public void RandomizeActivePoolPositions(float randomOffset)
+    {
+        if (activePool.Count == 0)
+        {
+            // No objects in the active pool, nothing to randomize
+            return;
+        }
+
+        float firstZ = activePool[0].transform.position.z; // Get the Z position of the first object
+
+        List<float> availableZPositions = new List<float>();
+
+        // Populate the availableZPositions list with increments
+        for (int i = 0; i < activePool.Count; i++)
+        {
+            availableZPositions.Add(i * randomOffset);
+        }
+
+        // Shuffle the list to randomize the order
+        for (int i = 0; i < availableZPositions.Count; i++)
+        {
+            float temp = availableZPositions[i];
+            int randomIndex = Random.Range(i, availableZPositions.Count);
+            availableZPositions[i] = availableZPositions[randomIndex];
+            availableZPositions[randomIndex] = temp;
+        }
+
+        // Assign each object to a unique Z position
+        for (int i = 0; i < activePool.Count; i++)
+        {
+            if (activePool[i] != null)
+            {
+                // Set the object's position
+                float zPosition = availableZPositions[i] + firstZ; // Add the starting Z position
+                activePool[i].transform.position = new Vector3(nextLocation.value.x, nextLocation.value.y, zPosition);
+                Debug.Log(zPosition);
+            }
+        }
+    }
 }
